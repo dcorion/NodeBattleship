92d478e3457c8f10a4cd1f963ce161c8ff264613
Ryan Barth <ryanbindc@gmail.com>
2023-07-07 00:55:02 -0400

    http

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var http = require('https').Server(app);
-var io = require('socket.io')(https);
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 var Entities = require('html-entities').AllHtmlEntities;
 var entities = new Entities();
 
